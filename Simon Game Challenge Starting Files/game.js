4d12970cd41c5307cd8718c521e229884d4fbaf1
Volodymyr Zomber <volodymyr.zomber@trinetix.com>
2020-06-23 19:08:24 +0300

    Done Simon

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,14 +2,19 @@ let buttonColours = ["red", "blue", "green", "yellow"]; // Possible colours
 let gamePattern = [];									// Saved randomed colours shown to user
 let userClickedPattern = [];							// User picker colour
 let level = 0;
+let gameIsOn = false;
 
 //-------------------------HANDLERS----------------------------//
 
 $(document).keydown(function(event) {
 	// Handler for start game by pressing any key
-	if (gamePattern.length === 0) {
+	if (!gameIsOn) {
+		gameIsOn = true;
+		gamePattern = [];
+		userClickedPattern = [];
 		nextSequence();
-		$("h1").text("Level 0");
+		$("h1#level-title").text("Level 0");
+		level = 0;
 	}
 });
 
@@ -21,23 +26,40 @@ $("div.btn").click(function () {
 	animatePress(userChosenColour);
 
 	// Check check answer
-	// checkAnswer(userClickedPattern.length - 1);
-	// console.log(userClickedPattern);
+	if (gameIsOn)
+		checkAnswer(userClickedPattern.length - 1);
 })
 
 //--------------------FUNCTIONS------------------------//
 
-// function checkAnswer(currentLevel) {
-// 	if (currentLevel === gamePattern[-1]) {
-// 		console.log("success");
-// 		setTimeout(nextSequence(), 1000);
-// 	}
-// 	else
-// 		console.log("fail") && playSound("wrong");
-// }
+function checkAnswer(currentLevel) {
+	console.log("Game -> " + gamePattern);
+	console.log("User -> " + userClickedPattern);
+
+	if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+		console.log("success");
+
+		if (userClickedPattern.length === gamePattern.length) {
+			// After checking answer, if correct, increase Level
+			setTimeout(function() {
+				$("h1#level-title").text("Level "+ ++level);
+				nextSequence();
+			}, 1000);
+		}
+	}
+	else {
+		console.log("fail");
+		$('body')[0].classList.add("game-over");
+		setTimeout(function () {$('body')[0].classList.remove("game-over");}, 200);
+		$("h1#level-title").text("Game Over, Press Any Key to Restart");
+		playSound("wrong");
+		gameIsOn = false;
+	}
+}
 
 function nextSequence() {
-	// userClickedPattern = []
+	// Set user choise empty
+	userClickedPattern = [];
 	// Get random colour
 	let randomNumber = Math.floor((Math.random() * 4));
 	let randomChosenColour = buttonColours[randomNumber];
@@ -46,19 +68,13 @@ function nextSequence() {
 	// Animate and play sound while showing next button to press
 	$("#" + randomChosenColour).fadeOut(150).fadeIn(150);
 	playSound($("#" + randomChosenColour).attr('id'));
-
-	// Update level on every sequence
-	$("h1").text("Level "+ level);
-	level++;
-
-	// console.log(gamePattern);
 }
 
 function animatePress(currentColour) {
 	// Animate user-pressed click on buttons with adding class and removing it later
 	$("#" + currentColour)[0].classList.add("pressed");
 
-	setTimeout(function() {
+	setTimeout(function () {
 		$("#" + currentColour)[0].classList.remove("pressed");
 	}, 100);
 }
